cfd3745d3f9924eecaadb5f3105d1e37a1608862
phuproz56 <phuproz348@gmail.com>
2023-07-19 16:59:04 +0700

    update setup database 1

---
+++ Friendify-App-Backend/src/setupDatabase.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+export default () => {
+    const connect = () => {
+        mongoose.connect('mongodb://127.0.0.1:27017/friendifyapp-backend')
+            .then(() => {
+                console.log('Successfully connected to database!!!');
+            })
+            .catch((error) => {
+                console.log('Error connecting to database...', error);
+                return process.exit(1);
+            })
+    };
+    connect();
+
+    mongoose.connection.on('disconnected', connect);
+};
